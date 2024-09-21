bbbf0780c5ef2727b549d954f6f518e43dd21a7e
barthe653 <barthselemani181@gmail.com>
2024-09-21 21:01:29 +0200

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,17 +1,12 @@
 data "aws_ami" "ubuntu" {
   most_recent = true
-
+  owners      = ["099720109477"]  # ID du propriétaire pour les AMI Ubuntu
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = [099720109477]
 }
+
 
 resource "aws_vpc" "vpc" {
   cidr_block = "172.16.0.0/16"
