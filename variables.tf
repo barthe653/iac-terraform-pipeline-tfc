d542a4ed83ae817901fd96d5a4ef52c9cf2ab463
barthe653 <barthselemani181@gmail.com>
2024-09-21 13:54:21 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "aws-owners" {}
 default = "099720109477"
 
 
-##variable "azure_subscription_id" {}
+###variable "azure_subscription_id" {}
 ##variable "azure_client_id" {}
 ##variable "azure_client_secret" {}
 ##variable "azure_tenant_id" {}
