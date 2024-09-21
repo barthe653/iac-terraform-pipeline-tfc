fe2f7e1a2781de8e2d92ded15f9f3339f2e541e8
barthe653 <barthselemani181@gmail.com>
2024-09-21 14:12:31 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,9 +2,9 @@ variable "aws_region" {
   default = "us-east-2"
 }
 
-variable "aws-owners" {
+variable "aws-owners" {}
 default = "099720109477"
-}
+
 
 ###variable "azure_subscription_id" {}
 ##variable "azure_client_id" {}
