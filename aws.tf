92a073511fc5ac2a9bb03808d7f36333f44fa326
barthe653 <barthselemani181@gmail.com>
2024-09-21 20:10:41 +0200

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -69,13 +69,13 @@ resource "aws_instance" "server-web" {
 
 
 
-##module "web-server-app" {
-##source                = "./modules/aws/web-server"
-##aws-owners            = [var.aws-owners]
-##aws_region            = var.aws_region
-##instance_type         = "t2.micro"
-##server_name           = "web-server-app"
-##vpc_cidr_block        = "11.10.0.0/16"
-##vpc_subnet_cidr_block = "11.10.1.0/28"
-##web_server_port       = "8081"
-##}
+module "web-server-app" {
+source                = "./modules/aws/web-server"
+aws-owners            = [var.aws-owners]
+aws_region            = var.aws_region
+instance_type         = "t2.micro"
+server_name           = "web-server-app"
+vpc_cidr_block        = "11.10.0.0/16"
+vpc_subnet_cidr_block = "11.10.1.0/28"
+web_server_port       = "8081"
+}
